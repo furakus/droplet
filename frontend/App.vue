6e5cc73af6e8626128237d7c116f9c84589b1d20
PZ Read <netfirewall@gmail.com>
2017-06-21 10:22:32 +0800

    Add drop file. Refactor state machine

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,33 +1,35 @@
 <template>
-<div id="app">
+<div id="app" @dragover.prevent="onDragFile" @drop.prevent="onDropFile">
     <div id="header" class="grid grid-center"></div>
     <div id="container" class="grid grid-center">
         <div class="col-6">
             <div class="grid"><div class="col">
                 <form-text :readonly="true" v-model="file_name">FILE</form-text>
             </div></div>
-            <div class="grid" v-if="link"><div class="col">
+            <div class="grid" v-if="state > 0"><div class="col">
                 <form-text :readonly="true" v-model="link">LINK</form-text>
             </div></div>
-            <div class="grid" v-if="link"><div class="col">
+            <div class="grid" v-if="state > 0"><div class="col">
                 <progress-bar :progress="progress"></progress-bar>
             </div></div>
-            <div class="grid" v-if="!file"><div class="col">
-                <button class="btn-warn" @click="$refs.fileinput.click()">SELECT</button>
-                <span class="guide">or drag and drop a file to upload.</span>
-            </div></div>
-            <div class="grid" v-if="file && !link"><div class="col">
-                <button class="btn-warn" @click="upload()">UPLOAD</button>
-                <button class="btn-defl" @click="reset()">CANCEL</button>
-                <button class="btn-harz" @click="$refs.fileinput.click()">CHANGE</button>
-            </div></div>
-            <div class="grid" v-if="link"><div class="col">
-                <button class="btn-harz" v-if="progress !== 1" @click="reset()">CANCEL</button>
-                <button class="btn-succ" v-if="progress === 1" @click="reset()">DONE</button>
+            <template v-if="state === 0">
+                <div class="grid" v-if="!file"><div class="col">
+                    <button class="btn-warn" @click="$refs.fileinput.click()">SELECT</button>
+                    <span class="guide">or drag and drop a file to upload.</span>
+                </div></div>
+                <div class="grid" v-if="file"><div class="col">
+                    <button class="btn-warn" @click="upload()">UPLOAD</button>
+                    <button class="btn-defl" @click="reset()">CANCEL</button>
+                    <button class="btn-harz" @click="$refs.fileinput.click()">CHANGE</button>
+                </div></div>
+            </template>
+            <div class="grid" v-if="state > 0"><div class="col">
+                <button class="btn-harz" v-if="state === 1" @click="reset()">CANCEL</button>
+                <button class="btn-succ" v-if="state === 2" @click="reset()">DONE</button>
             </div></div>
         </div>
     </div>
-    <input ref="fileinput" type="file" hidden="true" @change="selectFile($event.target.files)" />
+    <input ref="fileinput" type="file" hidden="true" @change="onSelectFile" />
 </div>
 </template>
 
@@ -49,8 +51,15 @@ interface ErrorResponse {
     msg: ErrorMessage
 }
 
+enum State {
+    Pending = 0,
+    Uploading = 1,
+    Done = 2,
+}
+
 @Component
 export default class App extends Vue {
+    state: State = State.Pending
     file: File | null = null
     link: string | null = null
     progress: number = 0
@@ -64,23 +73,42 @@ export default class App extends Vue {
         }
     }
 
-    selectFile(files: FileList) {
-        if (files.length != 1) {
-            return
+    onDragFile(evt: DragEvent) {
+        if (this.state === State.Done) {
+            this.reset()
         }
-        this.file = files[0]
+        if (this.state === State.Pending) {
+            evt.dataTransfer.dropEffect = 'copy'
+        } else {
+            evt.dataTransfer.dropEffect = 'none'
+        }
+    }
+
+    onDropFile(evt: DragEvent) {
+        let files = evt.dataTransfer.files
+        if (this.state === State.Pending && files.length > 0) {
+            this.file = files[0]
+        }
+    }
+
+    onSelectFile(evt: Event) {
+        let files = (<HTMLInputElement>evt.target).files
+        if (this.state === State.Pending && files !== null && files.length > 0) {  
+            this.file = files[0]
+        }
     }
 
     async upload() {
-        if (this.file === null) {
+        if (this.state !== State.Pending || this.file === null) {
             return
         }
+        let file = this.file
         let hasher = new Hashids(navigator.userAgent, 4, 'abcdefghijklmnopqrstuvwxyz0123456789')
         let id: string
         let data: UploadResponse | null = null
         while (true) {
             id = hasher.encode((new Date()).getTime())
-            let res = await Axios.post(`/api/id/${id}/upload`, { size: this.file.size })
+            let res = await Axios.post(`/api/id/${id}/upload`, { size: file.size })
             if (res.status === 200) {
                 data = res.data
                 break
@@ -93,7 +121,11 @@ export default class App extends Vue {
         }
         if (data !== null) {
             this.link = `${location.origin}/${id}/${this.file_name}`
-            this.uploadBlob(this.file, data.storage_server, data.flow_id, data.flow_token)
+            this.state = State.Uploading
+            await this.uploadBlob(file, data.storage_server, data.flow_id, data.flow_token)
+            this.state = State.Done
+        } else {
+            this.reset()
         }
     }
 
@@ -105,6 +137,7 @@ export default class App extends Vue {
             this.cancel_token.cancel()    
             this.cancel_token = null
         }
+        this.state = State.Pending
     }
 
     async uploadBlob(blob: Blob, storage_server: string, flow_id: string, flow_token: string) {
@@ -116,7 +149,6 @@ export default class App extends Vue {
             onUploadProgress: progressCallback,
             cancelToken: this.cancel_token.token,
         })
-        this.progress = 1
     }
 }
 </script>
@@ -132,23 +164,19 @@ body {
 
 #app {
     height: 100%;
-    max-width: 768px;
-    margin: 0 auto;
+    width: 100%;
 }
 
 #header {
     height: 2rem;
-    > div.title {
-        line-height: 2rem;
-        background-color: #012756;
-        color: white;
-        font-family: 'Source Code Pro', monospace;
-        text-align: center;
-    }
+    max-width: 768px;
+    margin: 0 auto;
 }
 
 #container {
     height: ~"calc(100% - 2rem)";
+    max-width: 768px;
+    margin: 0 auto;
     span.guide {
         margin-left: 0.5rem;
         .mono-font(0.8rem);
