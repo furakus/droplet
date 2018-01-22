fad6a659e49bd24c020845cdee9edeb2a55ec1d9
PZ Read <netfirewall@gmail.com>
2018-01-23 01:22:02 +0800

    Add compressor cancel

--- website/src/components/FileCompressor.tsx
+++ website/src/components/FileCompressor.tsx
@@ -14,28 +14,39 @@ interface FileCompressorState {
 }
 
 export class FileCompressor extends React.Component<FileCompressorProps, FileCompressorState> {
-    zipfile: JSZip
+    cancel_callback: () => void
     constructor(props: FileCompressorProps) {
         super(props)
         this.state = {
             progress: 0
         }
-        this.zipfile = new JSZip()
-        for (let file of this.props.filelist) {
-            this.zipfile.file(file.name, file)
-        }
-        this.zipfile.generateAsync({type: 'blob'}, this.onProgress).then(this.props.onDone)
     }
-    onProgress = (metadata: {percent: number}) => {
+    componentWillMount() {
+        let zipfile = new JSZip()
+        for (let file of this.props.filelist) {
+            zipfile.file(file.name, file)
+        }
+        new Promise<Blob>((resolve, reject) => {
+            this.cancel_callback = reject
+            zipfile.generateAsync({type: 'blob'}, this.onProgress).then(resolve)
+        }).then(this.props.onDone).catch(() => {})
+    }
+    componentWillUnmount() {
+        this.cancel_callback()
+    }
+    onCancel = () => {
+        this.cancel_callback()
+        this.props.onCancel()
+    }
+    onProgress = (metadata: {percent: number, currentFile: string}) => {
         this.setState({progress: metadata.percent})
     }
     render() {
         return (<div className="col-md-8 col-lg-6">
-            <span className="mb-2">Compressing</span>
             <div className="progress mb-2">
                 <div className="progress-bar" style={{width: `${this.state.progress}%`}} role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
-            <button className="btn">Cancel</button>
+            <button className="btn" onClick={this.onCancel}>Cancel</button>
         </div>)
     }
 }
