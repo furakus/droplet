135d0dd72bf9e1008cfa6fa5e9c65bbe7555576c
PZ Read <netfirewall@gmail.com>
2018-04-08 22:32:37 +0800

    Clean dist

--- backend/dist/inc/interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ErrorMessage;
-(function (ErrorMessage) {
-    ErrorMessage["INVALID_PARAM"] = "Invalid Parameter";
-    ErrorMessage["INVALID_ID"] = "Invalid ID";
-    ErrorMessage["DUPLICATED_ID"] = "Duplicated ID";
-    ErrorMessage["INTERNAL"] = "Internal Error";
-})(ErrorMessage = exports.ErrorMessage || (exports.ErrorMessage = {}));
